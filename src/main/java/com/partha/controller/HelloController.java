76985145c5025c43f2cf594e196f761f39157a2c
Parthasaradhi <parthasaradhi.maddirala91@gmail.com>
2022-06-13 12:23:36 +0530

    intial commi

---
+++ src/main/java/com/partha/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.partha.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String helloExample() {
+		return "Hello Partha, Welcome to Springboot";
+	}
+
+}
